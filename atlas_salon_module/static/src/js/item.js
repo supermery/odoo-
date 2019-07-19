eb01e88b1b3716a3a8aa0c7d8cd6473e41f5b216
meryam_daoudi <era@live.fr>
2019-07-19 21:19:34 +0100

    version 3

--- atlas_salon_module/static/src/js/item.js
+++ atlas_salon_module/static/src/js/item.js
@@ -1,10 +1,11 @@
 
 
-
+function myfunction({
 $(document).ready(function(){
    $(document).on("click",".login-button",function(){
-     var form = $(this).closest("form");
-     //console.log(form);
-     form.submit();
+//     var form = $(this).closest("form");
+     console.log(form);
+//     form.submit();
    });
+});
 });
